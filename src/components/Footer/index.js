0d2aa357df80695b3e26c5be05a2d138025274ad
Samuli Jarvinen <frye@github.com>
2022-05-09 19:54:43 -0700

    First version of footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,31 @@
+import "./index.scss";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <ul>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/samulijarvinen/">
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://github.com/frye">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://youtube.com">
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
