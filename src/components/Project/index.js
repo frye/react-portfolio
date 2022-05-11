f86c4c084c620e5f4a0ec307dfd6798472bd346a
Samuli Jarvinen <frye@github.com>
2022-05-11 02:34:24 -0700

    BAsic project card functionality and json file to control data

---
+++ src/components/Project/index.js
@@ -0,0 +1,35 @@
+import "./index.scss";
+
+const Project = (props) => {
+  return (
+    <div className="card">
+      <h1 className="card-title">{props.project.name}</h1>
+      <img
+        className="card-image"
+        src={props.project.image}
+        alt={props.project.name}
+      />
+      <p className="card-text">{props.project.description}</p>
+      <div className="card-links">
+        <a
+          href={props.project.github_link}
+          rel="noreferrer"
+          target="_blank"
+          className="card-link"
+        >
+          Check the code on GitHub
+        </a>
+        <a
+          href={props.project.deployed_link}
+          rel="noreferrer"
+          target="_blank"
+          className="card-link"
+        >
+          Visit the project
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,3 +1,15 @@
-const Projects = () => ('Hello Projects');
+import Project from '../Project';
+import './index.scss';
+const projectsJson = require('../../assets/projects.json');
+const Projects = () => {
+  return (
+    <div className="projects-container">
+      {projectsJson.map(
+        (project) => (<Project key={project.name} project={project} />)
+      )}
+    </div>
+  );
+};
+
 
   export default Projects;
